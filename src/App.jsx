eaca1a5c822fbd4fde5db5b310fc5f227c416efb
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-02-08 11:56:37 +0000

    moved api key inside of try

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,11 @@ function App() {
 
     async function getWeatherDataFromApi () {
       // ^ to use async in useEffect make an async function and then immediately call it..
-      
-      const APIKEY = `51178a0aeac643629d5204449230702`;
-      // ^ this should really be stored in an .env file
 
       try {
+        const APIKEY = `51178a0aeac643629d5204449230702`;
+        // ^ this should really be stored in an .env file
+        
         const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=${city}&aqi=no`);
         const data = await response.json();
         console.log(`fetch result:`, data);
